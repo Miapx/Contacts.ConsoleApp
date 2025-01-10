af2175e1f0fe66572282294bd958089ace754f5a
Mia <mihangrahn@gmail.com>
2025-01-10 10:44:07 +0100

    IdGenerator_Test tillagt

---
+++ Contacts.ConsoleApp.Tests/Helpers/IdGenerator_Test.cs
@@ -0,0 +1,18 @@
+﻿using Business.Helpers;
+
+namespace Contacts.ConsoleApp.Tests.Helpers
+{
+
+    public class IdGenerator_Tests
+    {
+        [Fact]
+        public void Generate_ShouldReturnGuidAsString()
+        {
+            var result = IdGenerator.Generate();
+
+            Assert.NotNull(result);
+            Assert.True(Guid.TryParse(result, out _));
+        }
+    }
+}
+
